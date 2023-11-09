8fc660f14ddc817c6b713fd00e3a2212b11ee589
anatyagi05 <atyagi21@asu.edu>
2023-11-09 13:12:18 -0700

    fixed accountCreate and did some css

--- accountCreate.js
+++ accountCreate.js
@@ -1,6 +1,4 @@
-// Load the user credentials from local storage or create an empty object if not found
-const users = JSON.parse(localStorage.getItem('users')) || {};
-
+// Function to create a user account
 function createAccount() {
     const firstName = document.querySelector('input[name="firstName"]').value;
     const lastName = document.querySelector('input[name="lastName"]').value;
@@ -8,17 +6,20 @@ function createAccount() {
     const username = document.querySelector('input[name="username"]').value;
     const password = document.querySelector('input[name="password"]').value;
 
+    // Retrieve the stored user data from local storage
+    const existingUsers = JSON.parse(localStorage.getItem('users')) || {};
+
     // Check if the username already exists
-    if (username in users) {
+    if (username in existingUsers) {
         alert('Username already exists. Please choose a different one.');
     } else {
         // Add the new user to the stored data
-        users[username] = password;
+        existingUsers[username] = password;
 
         // Save the updated user data back to local storage
-        localStorage.setItem('users', JSON.stringify(users));
-   
-        window.location.href = 'login.html';
+        localStorage.setItem('users', JSON.stringify(existingUsers));
+
+        
     }
 }
 
